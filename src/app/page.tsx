8991289eee06b788d931f1bfc7f7f47ccb6e71ea
france <frongrider2@gmail.com>
2024-10-31 17:15:16 +0700

    add console

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
+console.log("endpoint", process.env.NEXT_PUBLIC_API_URL);
+
 export default function Home() {
   const { push } = useRouter();
   useEffect(() => {
